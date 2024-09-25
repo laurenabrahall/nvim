815b5a70a8f3c8276de71e3ba7e59ce325fbd6e1
Lauren Abrahall <laurenab@amazon.com>
2024-09-25 23:35:44 +0200

    Add which-key support

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -32,6 +32,6 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format File" })
   end,
 }
