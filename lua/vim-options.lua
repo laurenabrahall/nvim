9f60181789c7c296a5627c7cb224d9700a0aa0ff
Lauren Abrahall <laurenab@amazon.com>
2024-09-25 23:36:37 +0200

    Add line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set hlsearch")
